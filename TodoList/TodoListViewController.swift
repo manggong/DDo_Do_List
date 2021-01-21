c9c9521dbe5eb0e43f9e2f733bf86653b28c00f8
manggong <jihwank1995@gmail.com>
2021-01-21 21:53:51 +0900

    add keyboard swap, touch area

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -25,8 +25,8 @@ class TodoListViewController: UIViewController {
         super.viewDidLoad()
         
         // TODO: 키보드 디텍션
-        
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView) , name: UIResponder.keyboardWillShowNotification , object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView) , name: UIResponder.keyboardWillHideNotification , object: nil)
         // TODO: 데이터 불러오기
         toDoListViewModel.loadTasks()
     }
@@ -43,13 +43,23 @@ class TodoListViewController: UIViewController {
     }
     
     // TODO: BG 탭했을때, 키보드 내려오게 하기
+    @IBAction func tapBG(_ sender: Any) {
+        inputTextField.resignFirstResponder()
+    }
 }
 
 extension TodoListViewController {
     @objc private func adjustInputView(noti: Notification) {
         guard let userInfo = noti.userInfo else { return }
         // TODO: 키보드 높이에 따른 인풋뷰 위치 변경
+        guard let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {return}
         
+        if noti.name == UIResponder.keyboardWillShowNotification {
+            let adjustmentHeight = keyboardFrame.height - view.safeAreaInsets.bottom
+            inputViewBottom.constant = adjustmentHeight
+        } else {
+            inputViewBottom.constant = 0
+        }
     }
 }
 
